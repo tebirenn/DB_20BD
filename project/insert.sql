4aba794f0abcb6f0a83925ccc401ee66c755ca5c
Alan <a_saudabekov@kbtu.kz>
2021-12-04 23:55:08 +0600

    project updates

--- project/insert.sql
+++ project/insert.sql
@@ -3,7 +3,7 @@ VALUES (1, 'Apple', 'Almaty', 'Abay', '12'),
        (2, 'Technodom', 'Almaty', 'Rayimbek batyr', '150'),
        (3, 'Mechta', 'Shymkent', 'Rozybakieva', '3'),
        (4, 'Samsung', 'Taraz', 'Tole bi', '59'),
-       (5, 'Sulpak', 'Almaty', 'Turgut Ozal', '80'),
+       (5, 'Sulpak', 'Almaty', 'Turgut Ozal', '80');
 
 INSERT INTO customer(id, first_name, second_name, age, login, password, phone_number, credit_card_id, subs_id, city, street_name, street_number, store_id)
 VALUES (1, 'Alan', 'Saudabekov', 19, 'youngdamn9', '123', '7702', 1, 1, 'Shymkent', 'Mustafayev', 14, 1),
@@ -13,19 +13,25 @@ VALUES (1, 'Alan', 'Saudabekov', 19, 'youngdamn9', '123', '7702', 1, 1, 'Shymken
 INSERT INTO subscription(id, cost, discount)
 VALUES (1, 2, 12),
        (2, 5, 15),
-       (3, 10, 5)
+       (3, 10, 5);
 
 INSERT INTO credit_card(id, producer, owner_name, owner_surname, date_of_issue, CVV)
 VALUES (1, 'Kaspi', 'Alan', 'Saudabekov', '16-06-2016', '764'),
        (2, 'Kaspi', 'Dimash', 'Saudabekov', '09-03-2018', '654'),
-       (3, 'Kaspi', 'Abay', 'Saudabekov', '23-12-2020', '318')
+       (3, 'Kaspi', 'Abay', 'Saudabekov', '23-12-2020', '318');
 
 INSERT product(id, name, price, description, manufacturer, qty, category)
 VALUES (1, 'Iphone 13', 500000, 'This is phone', 'Apple', 3, 'Smartphone'),
        (2, 'Samsung A51', 200000, 'This is phone', 'Samsung', 3, 'Smartphone'),
-       (3, 'Macbook pro', 500000, 'M1', 'Apple', 3, 'Notebook')
+       (3, 'Macbook pro', 500000, 'M1', 'Apple', 3, 'Notebook');
 
 INSERT INTO employee(id, first_name, second_name, age, phone_number, city, street_name, street_number)
 VALUES (1, 'Dias', 'Kassym', 20, '7700', 'Almaty', 'pass', 45),
        (2, 'Nurai', 'Senalova', 19, '7777', 'Almaty', 'pass', 78),
-       (3, 'Egor', 'Vladov', 21, '7747', 'Almaty', 'pass', 21)
+       (3, 'Egor', 'Vladov', 21, '7747', 'Almaty', 'pass', 21);
+
+INSERT INTO orders(id, store_id, customer_id, total_price, payment_method, order_date, order_location, order_address)
+VALUES (1, 4, 1, 500000, 'card', '12-02-2021', 'Almaty', 'Shymkent');
+
+INSERT INTO delivery(id, service_name, delivery_address, price, sending_time, arrival_time, transport_id)
+VALUES (1, 'Delivery club', 'Shymkent', 5000, '12-02-2021 10:06:12', '13-02-2021 18:12:56');
